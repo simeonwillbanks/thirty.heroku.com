f3002bd980001d2bd73ddd08251b4d8dc7eb0a7b
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2011-02-25 10:44:51 -0800

    Move OAuth credentials to Heroku config-vars and set app constants from config-vars in initializers directory.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,7 @@
+
+# Twitter OAuth credentials from Heroku config-vars
+# http://devcenter.heroku.com/articles/config-vars
+OAUTH_CONSUMER_KEY        = ENV['OAUTH_CONSUMER_KEY']
+OAUTH_CONSUMER_SECRET     = ENV['OAUTH_CONSUMER_SECRET']
+OAUTH_ACCESS_TOKEN        = ENV['OAUTH_ACCESS_TOKEN']
+OAUTH_ACCESS_TOKEN_SECRET = ENV['OAUTH_ACCESS_TOKEN_SECRET']
--- lib/twitter_oauth_consumer_lite.rb
+++ lib/twitter_oauth_consumer_lite.rb
@@ -2,17 +2,12 @@ require "oauth/consumer"
 
 module TwitterOAuthConsumerLite
   
-  CONSUMER_KEY = "NqxQTQEo2o8GeNu5hp4Okw"
-  CONSUMER_SECRET = "UxhB8iH02CSl1vhAcPVdhuT1EIeciDubmjG5RrtI"
-  ACCESS_TOKEN = "186658203-hLY80X84xUcnEiHLYo4DzMoCZ8tFgW5hLsKySzwL"
-  ACCESS_TOKEN_SECRET = "kxwjqW3kXnF2yFWhcYAKeUCpLmvpStcig4t3MkOrM"
-  
-  # Exchange ACCESS_TOKEN and ACCESS_TOKEN_SECRET for an AccessToken instance
+  # Exchange OAUTH_ACCESS_TOKEN and OAUTH_ACCESS_TOKEN_SECRET for an AccessToken instance
   def self.access_token
-    consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, 
+    consumer = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, 
                {:site => "http://api.twitter.com", :scheme => :header})
-    token_hash = {:oauth_token => ACCESS_TOKEN, 
-                  :oauth_token_secret => ACCESS_TOKEN_SECRET}
+    token_hash = {:oauth_token => OAUTH_ACCESS_TOKEN, 
+                  :oauth_token_secret => OAUTH_ACCESS_TOKEN_SECRET}
     OAuth::AccessToken.from_hash(consumer, token_hash)
   end
 
